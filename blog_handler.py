36aea72f40ed6861fbb9acd03309bb5ef44e7eeb
Kenneth LaMantia <kennethlamantia@gmail.com>
2017-01-16 08:53:49 -0500

    Refactor so that form input methods are contained in form input helper class. Refactor to use app engine ndb library rather than db. Begin implementing login handler.

--- blog_handler.py
+++ blog_handler.py
@@ -8,7 +8,7 @@ import re
 
 import jinja2
 import webapp2
-from google.appengine.ext import db
+from google.appengine.ext import ndb
 import cookie_validator
 
 TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), 'templates')
@@ -18,8 +18,9 @@ JINJA = jinja2.Environment(loader = jinja2.FileSystemLoader(TEMPLATE_DIR),
 MAIN_PAGE = "blog.html"
 NEW_POST = "newpost.html"
 NEW_POST_DISPLAY = "new_post_display.html"
-SIGNUP_TEMPLATE = "login_template.html"
+SIGNUP_TEMPLATE = "signup_page.html"
 WELCOME_TEMPLATE = "welcome.html"
+LOGIN_TEMPLATE = "login_page.html"
 
 # Add regex for form input verification
 INPUTS = {"subject" : "^.{1,100}$",
@@ -36,19 +37,20 @@ class Handler(webapp2.RequestHandler):
     def render(self, *a, **kw):
         self.write(self.render_str(*a, **kw))
 
-class BlogPost(db.Model):
+class BlogPost(ndb.Model):
     '''
     Class to describe the entity used to store blog entries in the data store.
     This entity has three properties, the title of the post, the text body 
     content of the post, and the time the post is created.
     '''
-    post_title = db.StringProperty(required = True)
-    post_body = db.TextProperty(required = True)
-    post_time = db.DateTimeProperty(auto_now_add=True)
+    post_title = ndb.StringProperty(required = True)
+    post_body = ndb.TextProperty(required = True)
+    post_time = ndb.DateTimeProperty(auto_now_add=True)
     
-class User(db.Model):
-    name = db.StringProperty(required = True)
-    password = db.StringProperty(required = True)
+class User(ndb.Model):
+    name = ndb.StringProperty(required = True)
+    password = ndb.StringProperty(required = True)
+    id = name
         
 class BlogMainPage(Handler):
     def get(self):
@@ -102,7 +104,7 @@ class NewPost(Handler):
         '''
         output_strings = {}
         for input in raw_inputs:
-            if not self.is_valid(raw_inputs[input], input):
+            if not self._is_valid(raw_inputs[input], input):
                 output_strings[input + "_error"] = "Invalid input."
                 output_strings[input] = raw_inputs.get(input)
                 self.valid_input = False
@@ -110,7 +112,7 @@ class NewPost(Handler):
                 output_strings[input] = raw_inputs.get(input)
         return output_strings
         
-    def is_valid(self, input, regex_key):
+    def _is_valid(self, input, regex_key):
         '''
         Helper function that returns a boolean value based on whether
         input string matches regex obtained from INPUTS with regex_key.
@@ -127,94 +129,160 @@ class NewPostDisplay(Handler):
                     content=current_body)
         
 class Signup(Handler):
+    # TODO add user to database when sign up. Check to make sure there isn't
+    # already another user with username/email
+    # otherwise login page will work even if you don't sign up!
     def __init__(self, request, response):
         self.initialize(request, response)
-        self.INPUT_MAP = {"username" : r"^[a-zA-Z0-9_-]{3,20}$",
+        self.input_to_regex = {"username" : r"^[a-zA-Z0-9_-]{3,20}$",
                           "password" : r"^.{3,20}$",
                           "verify" : r"^.{3,20}$",
-                          "email" : r"^[\S]+@[\S]+.[\S]+$"
+                          "email" : r"(^[\S]+@[\S]+.[\S]+$)|(^(?![\s\S]))"
                           }
+        self.input_to_error = {"username" : "The username is invalid.",
+                               "password" : "The password is invalid.",
+                               "verify" : "The verification field is invalid.",
+                               "email" : "Invalid email address."}
 
     def get(self):
         self.render(SIGNUP_TEMPLATE)
         
     def post(self):
-        self.valid_input = True
-        self.output_map = dict()
-        input_map = self.get_inputs(self.INPUT_MAP.iterkeys())
-        self.validate_form_data(input_map, self.output_map)
-        self.passwords_match(input_map, self.output_map)
-        to_render = self.output_map
+
+        helper = FormInputHelper(self.input_to_regex.iterkeys(),
+                                 self.input_to_regex,
+                                 self.input_to_error,
+                                 self,
+                                 True)
+        to_render = helper.process_form_input()
         
-        if not self.valid_input:
+        if not helper.valid_input:
             self.render(SIGNUP_TEMPLATE, **to_render)
         else:
-            self.set_cookie("name", to_render.get("username"))
-            self.set_cookie("password", to_render.get("password"))
+            cookie_helper = cookie_validator.Validator(self)
+            cookie_helper.set_cookie("name", to_render.get("username"))
+            cookie_helper.set_cookie("password", to_render.get("password"))
             self.redirect("/blog/welcome")
     
-    def set_cookie(self, cookie_name, cookie_value):
-        self.response.headers.add_header(
-            "Set-Cookie", cookie_validator.format_cookie(cookie_name, 
-                                                        cookie_value))
+class Welcome(Handler):
+    def get(self):
+        cookie_helper = cookie_validator.Validator(self)
+        name_cookie = self.request.cookies.get("name")
+        password_cookie = self.request.cookies.get("password")
+        if name_cookie and password_cookie:
+            if (cookie_helper.validate_hash(name_cookie) and 
+                                cookie_helper.validate_hash(password_cookie)):
+                username = cookie_helper.get_value(name_cookie)
+                self.render(WELCOME_TEMPLATE, username = username)
+        else:
+            self.redirect("/blog/login")
+                
+class Login(Handler):
+    # add check to make sure user is in database
+    def get(self):
+        self.render(LOGIN_TEMPLATE)
     
-    def is_valid(self, data, re_input_key):
-        '''
-        If returns true if the data is valid, false otherwise.
-        '''
-        pattern = re.compile(self.INPUT_MAP[re_input_key])
-        return pattern.match(data)
+    def post(self):
+        form_fields = ["username",
+                            "password"]
+        regex_map = {"username" : r"^[a-zA-Z0-9_-]{3,20}$",
+                          "password" : r"^.{3,20}$"}
+        error_map = {"username" : "Please enter a username.",
+                     "password" : "Please enter a password."}
+        helper = FormInputHelper(form_fields, regex_map, error_map, self)
+        to_render = helper.process_form_input()
+        if not helper.valid_input:
+            self.render(LOGIN_TEMPLATE, **to_render)
+        else:
+            cookie_helper = cookie_validator.Validator(self)
+            cookie_helper.set_cookie("name", to_render.get("username"))
+            cookie_helper.set_cookie("password", to_render.get("password"))
+            self.redirect("/blog/welcome")
+        
+class FormInputHelper(object):
+    '''
+    Class to help process form input.
     
-    def get_inputs(self, input_list):
+    form_fields - a list of the names of form fields. Need to be strings.
+    form_regex - a dictionary that maps the name of the form field to the 
+                regex matching acceptable input to that field.
+    form_errors - dictionary mapping form field names to error messages
+    is_signup_form - optional flag, will
+    handler_obj - an instance of the webapp2 handler object using this class.
+    '''
+    def __init__(self, form_fields, form_regex, form_errors, handler_obj,
+                 is_signup_form = None):
+        self.form_fields = {}
+        for field_name in form_fields:
+            self.form_fields[field_name] = None
+        self.form_errors = form_errors
+        self.form_regex = form_regex
+        self.valid_input = True
+        self.output_map = {}
+        self.is_signup_form = is_signup_form
+        self.handler_obj = handler_obj
+        
+    def process_form_input(self):
         '''
-        Gets the data entered in the text and password inputs. Stores the input
-        in a dictionary.
-        INPUT_MAP key : string value entered.
+        Rename this method.
         '''
-        output_map = dict()
-        for input in input_list:
-            output_map[input] = self.request.get(input)
-        return output_map
+        self.get_inputs()
+        self.validate_form_data()
+        if self.is_signup_form:
+            self.passwords_match()
+        return self.output_map
+        
+    def _is_valid(self, form_data, field_name):
+        '''
+        Helper method.
+        If returns true if the form_data is valid, false otherwise.
+        field_name - name of the form field.
+        '''
+        pattern = re.compile(self.form_regex[field_name])
+        return pattern.match(form_data)
     
-    def validate_form_data(self, input_map, output_map):
+    def get_inputs(self):
+        '''
+        Uses a GET request of handler object to populate the form_fields
+        dictionary with the input using put in the form.
+        '''
+        for field in self.form_fields:
+            self.form_fields[field] = self.handler_obj.request.get(field)
+        
+    
+    def validate_form_data(self):
         '''
         Checks each input form element to make sure it matches the specified
         requirements and updates the appropriate form output. If the input
         is not correct, generates an error message.
         '''
-        for key in input_map:
-            if not self.is_valid(input_map[key], key):
-                if key == "email" and input_map[key] == "":
-                    continue
-                self.valid_input = False
-                output_map[key + "_error"] = self.generate_invalid_response(key)
-                output_map[key] = ""
-            else:
-                output_map[key] = input_map[key]
+        for field_name in self.form_fields:
+            if not self._is_valid(self.form_fields.get(field_name), 
+                                  field_name):
                 
-    def passwords_match(self, input_map, output_map):
+                self.valid_input = False
+                self.output_map[field_name + "_error"] = self.form_errors.get(
+                                                                    field_name)
+                self.output_map[field_name] = ""
+            else:
+                self.output_map[field_name] = self.form_fields[field_name]
+    
+    def passwords_match(self):
         '''
+        This method only works if the optional is_signup_form argument is 
+        passed to the constructor.
         Check whether input password and verify fields match.
         '''
-        if not input_map["password"] == input_map["verify"]:
-            output_map["password_error"] = "Passwords don't match"
+        if not self.form_fields["password"] == self.form_fields["verify"]:
+            self.output_map["password_error"] = "Passwords don't match"
             self.valid_input = False
-        
-    def generate_invalid_response(self, input_type):
-        response = "That's not a valid {type}."
-        return response.format(type = input_type)
     
-class Welcome(Handler):
-    def get(self):
-        name_cookie = self.request.cookies.get("name")
-        if name_cookie:
-            if cookie_validator.validate_hash(name_cookie):
-                username = cookie_validator.get_value(name_cookie)
-                self.render(WELCOME_TEMPLATE, username = username)  
+          
         
-app = webapp2.WSGIApplication([('/blog', BlogMainPage),
-                               ('/blog/newpost', NewPost),
-                               (r'/blog/post_id/(\d+)', NewPostDisplay),
-                               ('/blog/signup', Signup),
-                               ('/blog/welcome', Welcome)], debug=True)
+app = webapp2.WSGIApplication([("/blog", BlogMainPage),
+                               ("/blog/newpost", NewPost),
+                               (r"/blog/post_id/(\d+)", NewPostDisplay),
+                               ("/blog/signup", Signup),
+                               ("/blog/welcome", Welcome),
+                               ("/blog/login", Login)], debug=True)
 
